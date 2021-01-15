012d70e92ae9c371eeb0045a3bba2dbe8adee5c1
KSK-47 <74639839+KSK-47@users.noreply.github.com>
2021-01-15 22:59:52 +0900

    first-commit

---
+++ ex09-2.js
@@ -0,0 +1,23 @@
+const list = document.getElementById('list')
+const newItem = document.createElement('li')
+newItem.textContent = 'アイテム1'
+list.appendChild(newItem)
+
+const addButton = document.getElementById('add')
+const removeButton = document.getElementById('remove')
+
+addButton.addEventListener('click', (event) => {
+    // createElement でliタグを生成
+    const newItem = document.createElement("li")
+    // listの子要素の個数を所得
+    const childno = list.children.length
+    // そのchildnoは最初0からスタートするので、childno+1をtextContentに追加
+    newItem.textContent = `アイテム${childno + 1}`
+    // listにliタグを追加
+    list.appendChild(newItem)
+})
+
+removeButton.addEventListener('click', (event) => {
+
+    list.removeChild(list.lastElementChild)
+})
